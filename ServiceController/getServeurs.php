944d89547724b1e2ad813aa655040299ba2884c9
Thomas <thomsguyot@gmail.com>
2025-05-28 10:17:27 +0200

    ajout

--- ServiceController/getServeurs.php
+++ ServiceController/getServeurs.php
@@ -14,6 +14,8 @@ $idService = $_POST['idService'];
 
 //appel DAO pour recuperer les serveurs présents ce jour-là pour ce service
 $dao = new UtilisateurDAO();
+$date = 2025-05-28;
+$idService = 1;
 $serveurs = $dao->getServeurs($date, $idService);
 
 echo json_encode($serveurs);
